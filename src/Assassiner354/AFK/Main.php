c9cb1645b3c18de217fabfb5f44cf33afb0e6359
Assassiner354 <40911019+A354-PH@users.noreply.github.com>
2019-07-02 19:19:02 +0800

    All fixed!

--- src/Assassiner354/AFK/Main.php
+++ src/Assassiner354/AFK/Main.php
@@ -28,46 +28,85 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 
 class Main extends PluginBase implements Listener {
 
-  public $afk = [];
-  
   public function onEnable() {
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->afk = array();
+
     @mkdir($this->getDataFolder());
+    $this->saveDefaultConfig();
+    $this->getResource("config.yml");
+		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
   }
-  
+
+  public function onJoin(PlayerJoinEvent $event){
+    $player = $event->getPlayer();
+    if(isset($this->afk[strtolower($player->getName())])) {
+      unset($this->afk[strtolower($player->getName())]);
+      $player->sendMessage(TF::GREEN . "You have been removed from AFK Mode!");
+    }
+  }
+
   public function onQuit(PlayerQuitEvent $event) {
     $player = $event->getPlayer();
-    if(in_array($this->afk[strtolower($player->getName())])) {
+    if(isset($this->afk[strtolower($player->getName())])) {
       unset($this->afk[strtolower($player->getName())]);
     }
   }
-  
+
   public function onMove(PlayerMoveEvent $event) {
     $player = $event->getPlayer();
     $name = $player->getName();
-    if(in_array($this->afk[strtolower($player->getName())])) {
-      unset($this->afk[strtolower($player->getName())]);
-      $player->setDisplayName($name);
-      $player->sendMessage(TF::GREEN . "You are no longer AFK!");
+    if(isset($this->afk[strtolower($player->getName())])) {
+      $player->sendMessage(TF::RED . "You can't move while AFK!");
+      $player->sendMessage(TF::GREEN . "Type /afk to start moving!");
+      $event->setCancelled(true);
     }
+    //DEPRECATED FEATURE! WILL BE REMOVED IN THE FUTURE
+    /*if($this->config->get("no-move") == "false") {
+      if(isset($this->afk[strtolower($player->getName())])) {
+        unset($this->afk[strtolower($player->getName())]);
+        $player->setDisplayName($name);
+        $player->sendMessage(TF::GREEN . "You are no longer AFK!");
+      }
+    }
+    if($this->config->get("no-move") == "true") {
+      if(isset($this->afk[strtolower($player->getName())])) {
+        $player->sendMessage(TF::RED . "You can't move while AFK!");
+        $player->sendMessage(TF::GREEN . "Type /afk to start moving!");
+        $event->setCancelled(true);
+      }
+    } */
   }
-  
+
   public function onChat(PlayerChatEvent $event) {
     $player = $event->getPlayer();
     $name = $player->getName();
-    if(in_array($this->afk[strtolower($player->getName())])) {
-      $event->setCancelled();
-      $player->sendMessage(TF::RED . "You can't chat while AFK!");
+    if($this->config->get("no-chat") == "false") {
+      if(isset($this->afk[strtolower($player->getName())])) {
+        unset($this->afk[strtolower($player->getName())]);
+        $player->setDisplayName($name);
+        $player->sendMessage(TF::GREEN . "You are no longer AFK!");
+      }
+    }
+    if($this->config->get("no-chat") == "true") {
+      if(isset($this->afk[strtolower($player->getName())])) {
+        $player->sendMessage(TF::RED . "You can't chat while AFK!");
+        $player->sendMessage(TF::GREEN . "Type /afk to start chatting!");
+        $event->setCancelled(true);
+      }
     }
   }
 
   public function onDamage(EntityDamageEvent $event) {
-    if($event->getEntity() instanceof Player && in_array($this->afk[strtolower($player->getName())])) {
-      $event->setCancelled(true);
+    if($event->getEntity() instanceof Player) {
+      if(isset($this->afk[strtolower($event->getEntity()->getName())])){
+        $event->setCancelled(true);
+      }
     }
   }
 
@@ -78,12 +117,12 @@ class Main extends PluginBase implements Listener {
           $sender->sendMessage(TF::RED . "This command can only be used in-game!");
           return true;
         }
-        
+
         $name = $sender->getName();
-        if(in_array($this->afk[strtolower($sender->getName())])) {
+        if(isset($this->afk[strtolower($sender->getName())])) {
           unset($this->afk[strtolower($sender->getName())]);
-          $player->setDisplayName($name);
-          $player->sendMessage(TF::GREEN . "You are no longer AFK!");
+          $sender->setDisplayName($name);
+          $sender->sendMessage(TF::GREEN . "You are no longer AFK!");
         } else {
           $this->afk[strtolower($sender->getName())] = strtolower($sender->getName());
           $sender->setDisplayName(TF::YELLOW . "[AFK] " . $name);
